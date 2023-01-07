0a74a7b99f15f4b82c469e61776bc988e5f88010
jjay31 <michalwojtowicz_2001@tlen.pl>
2023-01-07 03:16:39 +0100

    Add boolean to text conversion

--- src/main/java/pl/greenhillhotel/greenhillhotel/AvailableRoom.java
+++ src/main/java/pl/greenhillhotel/greenhillhotel/AvailableRoom.java
@@ -1,15 +1,29 @@
 package pl.greenhillhotel.greenhillhotel;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AvailableRoom extends SearchBean {
 
     private final int id_room;
+    private final String balconyText;
 
     public AvailableRoom(String accommodation, String checkout, int people, boolean balcony, int id_room) {
         super(accommodation, checkout, people, balcony);
         this.id_room = id_room;
+        balconyText = convertBooleanToText(balcony);
+    }
+
+    private String convertBooleanToText(boolean state) {
+        Map<Boolean, String> map = new HashMap<>();
+        map.put(true, "Tak");
+        map.put(false, "Nie");
+        return map.get(state);
     }
 
     public int getId_room() {
         return id_room;
     }
+
+    public String getBalconyText() {return balconyText;}
 }
